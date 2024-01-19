0c6e64abf8768d089cb6dc0557a1ce9fab387cda
Devynn Garrow <devynn.garrow@outlook.com>
2024-01-19 14:44:46 -0500

    Initial assignment files

---
+++ cagey_csp.py
@@ -0,0 +1,99 @@
+# =============================
+# Student Names:
+# Group ID:
+# Date:
+# =============================
+# CISC 352 - W23
+# cagey_csp.py
+# desc:
+#
+
+#Look for #IMPLEMENT tags in this file.
+'''
+All models need to return a CSP object, and a list of lists of Variable objects
+representing the board. The returned list of lists is used to access the
+solution.
+
+For example, after these three lines of code
+
+    csp, var_array = binary_ne_grid(board)
+    solver = BT(csp)
+    solver.bt_search(prop_FC, var_ord)
+
+var_array is a list of all variables in the given csp. If you are returning an entire grid's worth of variables
+they should be arranged in a linearly, where index 0 represents the top left grid cell, index n-1 represents
+the top right grid cell, and index (n^2)-1 represents the bottom right grid cell. Any additional variables you use
+should fall after that (i.e., the cage operand variables, if required).
+
+1. binary_ne_grid (worth 10/100 marks)
+    - A model of a Cagey grid (without cage constraints) built using only
+      binary not-equal constraints for both the row and column constraints.
+
+2. nary_ad_grid (worth 10/100 marks)
+    - A model of a Cagey grid (without cage constraints) built using only n-ary
+      all-different constraints for both the row and column constraints.
+
+3. cagey_csp_model (worth 20/100 marks)
+    - a model of a Cagey grid built using your choice of (1) binary not-equal, or
+      (2) n-ary all-different constraints for the grid, together with Cagey cage
+      constraints.
+
+
+Cagey Grids are addressed as follows (top number represents how the grid cells are adressed in grid definition tuple);
+(bottom number represents where the cell would fall in the var_array):
++-------+-------+-------+-------+
+|  1,1  |  1,2  |  ...  |  1,n  |
+|       |       |       |       |
+|   0   |   1   |       |  n-1  |
++-------+-------+-------+-------+
+|  2,1  |  2,2  |  ...  |  2,n  |
+|       |       |       |       |
+|   n   |  n+1  |       | 2n-1  |
++-------+-------+-------+-------+
+|  ...  |  ...  |  ...  |  ...  |
+|       |       |       |       |
+|       |       |       |       |
++-------+-------+-------+-------+
+|  n,1  |  n,2  |  ...  |  n,n  |
+|       |       |       |       |
+|n^2-n-1| n^2-n |       | n^2-1 |
++-------+-------+-------+-------+
+
+Boards are given in the following format:
+(n, [cages])
+
+n - is the size of the grid,
+cages - is a list of tuples defining all cage constraints on a given grid.
+
+
+each cage has the following structure
+(v, [c1, c2, ..., cm], op)
+
+v - the value of the cage.
+[c1, c2, ..., cm] - is a list containing the address of each grid-cell which goes into the cage (e.g [(1,2), (1,1)])
+op - a flag containing the operation used in the cage (None if unknown)
+      - '+' for addition
+      - '-' for subtraction
+      - '*' for multiplication
+      - '/' for division
+      - '?' for unknown/no operation given
+
+An example of a 3x3 puzzle would be defined as:
+(3, [(3,[(1,1), (2,1)],"+"),(1, [(1,2)], '?'), (8, [(1,3), (2,3), (2,2)], "+"), (3, [(3,1)], '?'), (3, [(3,2), (3,3)], "+")])
+
+'''
+
+from cspbase import *
+
+def binary_ne_grid(cagey_grid):
+    ##IMPLEMENT
+    pass
+
+
+def nary_ad_grid(cagey_grid):
+    ## IMPLEMENT
+    pass
+
+def cagey_csp_model(cagey_grid):
+    ##IMPLEMENT
+    pass
---
+++ heuristics.py
@@ -0,0 +1,38 @@
+# =============================
+# Student Names:
+# Group ID:
+# Date:
+# =============================
+# CISC 352 - W23
+# heuristics.py
+# desc:
+#
+
+
+#Look for #IMPLEMENT tags in this file. These tags indicate what has
+#to be implemented to complete problem solution.
+
+'''This file will contain different constraint propagators to be used within
+   the propagators
+
+var_ordering == a function with the following template
+    var_ordering(csp)
+        ==> returns Variable
+
+    csp is a CSP object---the heuristic can use this to get access to the
+    variables and constraints of the problem. The assigned variables can be
+    accessed via methods, the values assigned can also be accessed.
+
+    var_ordering returns the next Variable to be assigned, as per the definition
+    of the heuristic it implements.
+   '''
+
+def ord_dh(csp):
+    ''' return variables according to the Degree Heuristic '''
+    # IMPLEMENT
+    pass
+
+def ord_mrv(csp):
+    ''' return variable according to the Minimum Remaining Values heuristic '''
+    # IMPLEMENT
+    pass
